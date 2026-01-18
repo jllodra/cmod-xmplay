d55fd23fcfdc298ff1e5241f99bef841a4b96500
jllodra <jllodra@users.noreply.github.com>
2026-01-18 01:01:51 +0100

    added support for octamed

--- dllmain.cpp
+++ dllmain.cpp
@@ -109,7 +109,7 @@ static const std::set<std::string> xmplayFormats = {
 
 // all formats (xmplay + openmpt supported)
 static const std::set<std::string> allFormats = {
-    "mptm","mod","s3m","xm","it","669","amf","ams","c67","dbm","digi","dmf","dsm","dsym","dtm","far","fmt","imf","ice","j2b","m15","mdl","med","mms","mt2","mtm","nst","okt","plm","psm","pt36","ptm","sfx","sfx2","st26","stk","stm","stx","stp","symmod","ult","wow","gdm","mo3","oxm","umx","xpk","ppm","mmcmp"
+    "mptm","mod","s3m","xm","it","669","amf","ams","c67","dbm","digi","dmf","dsm","dsym","dtm","far","fmt","imf","ice","j2b","m15","mdl","med","mms","mt2","mtm","nst","okt","plm","psm","pt36","ptm","sfx","sfx2","st26","stk","stm","stx","stp","symmod","ult","wow","gdm","mo3","oxm","umx","xpk","ppm","mmcmp", "mmd0", "mmd1", "mmd2", "mmd3", "mmdc"
 };
 
 struct RebuildParams {
